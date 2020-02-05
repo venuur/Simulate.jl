3b9b516147edad4caa77d22aa603495c532f2cb9
Carl Morris <carl.morris.world@outlook.com>
2020-02-04 17:37:35 -0800

    Add basic functionality and example
    Add minimum simulation functionality via core simulate and gethistory
    accessor for simulation log data structure.

--- src/Simulate.jl
+++ src/Simulate.jl
@@ -1,5 +1,31 @@
 module Simulate
 
-greet() = print("Hello World!")
+using StructArrays
+
+export simulate, gethistory
+
+## API
+function simulate(step!, init, data, periods; verbose = false)
+    log = Log(init, periods)
+    state = deepcopy(init)
+    for i = 2:(periods+1)
+        verbose && println(state)
+        step!(state, data)
+        log[i] = deepcopy(state)
+    end
+    verbose && println(state)
+    return log
+end
+
+
+## Simulation log object
+function Log(init, periods)
+    log = StructArray{typeof(init)}(undef, periods + 1)
+    log[1] = deepcopy(init)
+    return log
+end
+
+# Accessors
+gethistory(log) = log
 
 end # module
---
+++ src/ex_inv.jl
@@ -0,0 +1,84 @@
+module ExInventory
+
+using Simulate
+using Parameters
+using DataStructures
+
+export simulate_constant_basestock
+
+struct OpenOrder
+    amount
+    arrival_time
+end
+
+@with_kw mutable struct InventoryState
+    time = 0
+    onhand = 0
+    inflight = 0
+    open_orders = Dict()  # PriorityQueue{OpenOrder.arrival_time, OpenOrder}
+    sales = 0
+    lost = 0
+    backordered = 0
+end
+
+struct InventoryData
+    demand
+    leadtime
+    get_order!
+end
+
+function backorder_inv_step!(state, data)
+    state.time += 1
+
+    # Handle arriving orders
+    if haskey(state.open_orders, state.time)
+        next_open_order = state.open_orders[state.time]
+        # Order has arrived.
+        state.onhand += next_open_order.amount
+        delete!(state.open_orders, state.time)
+        state.inflight -= next_open_order.amount
+    end
+
+    # Determine order prior to  sales arrival
+    to_order = data.get_order!(state)
+    if to_order > 0
+        leadtime = data.leadtime[state.time]
+        new_order = OpenOrder(to_order, state.time + leadtime)
+        if haskey(state.open_orders, new_order.arrival_time)
+            existing_order = state.open_orders[new_order.arrival_time]
+            state.open_orders[new_order.arrival_time] = OpenOrder(
+                existing_order.amount + new_order.amount,
+                new_order.arrival_time,
+            )
+        else
+            state.open_orders[new_order.arrival_time] = new_order
+        end
+        state.inflight += to_order
+    end
+
+    # Process arriving demand
+    demand = data.demand[state.time]
+    state.sales = min(state.onhand, demand)
+    state.backordered += max(demand - state.onhand, 0)
+    state.backordered = max(state.backordered - state.sales, 0)
+    state.onhand = max(state.onhand - demand, 0)
+
+    # No lost sales in this simulation.
+end
+
+struct ConstantBasestockPolicy
+    order_upto
+end
+
+(p::ConstantBasestockPolicy)(state) = p.order_upto - state.onhand - state.inflight
+
+function simulate_constant_basestock(init_inv, basestock, demand, leadtime; kwargs...)
+    @assert length(demand) == length(leadtime)
+    data = InventoryData(demand, leadtime, ConstantBasestockPolicy(basestock))
+    init = InventoryState(onhand = init_inv)
+    periods = length(demand)
+    return simulate(backorder_inv_step!, init, data, periods; kwargs...)
+end
+
+
+end # module
